94176bddd92590a85cce820399a7b71410f08a01
yamodah <yassinedahlek@gmail.com>
2021-11-03 23:37:06 -0400

    added imports and routes

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ shows a list of decks with options to create, study, view, or delete a deck
 import React from "react";
 
 function Home(){
-
+<button type="button" class="btn btn-secondary">Create Deck</button>
 }
 
 export default Home
